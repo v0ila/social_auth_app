f154bc1023848f584691e91c48cbe199b176c98a
v0ila <wesley_@live.nl>
2015-03-30 15:17:02 +0200

    DynamicTitle

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -12,6 +12,8 @@ Rails.application.configure do
   # preloads Rails for running tests, you may have to set it to true.
   config.eager_load = false
 
+  config.active_support.test_order = :sorted
+
   # Configure static asset server for tests with Cache-Control for performance.
   config.serve_static_assets  = true
   config.static_cache_control = 'public, max-age=3600'
