22d153cb0261a519995a48266eb2dea4407babd4
v0ila <wesley_@live.nl>
2015-03-30 18:55:50 +0200

    signup

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   get 'help'    => 'static_pages#help'
   get 'about'   => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
-  get 'login'   => 'static_pages#login'
+  get 'signup'  => 'users#new'
 end
