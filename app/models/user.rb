fae9e90d5b90c5973c440a570f14c102cd375fe2
v0ila <wesley_@live.nl>
2015-04-01 01:56:27 +0200

    Heroku Mailer

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  attr_accessor :remember_token, :activation_token
+  attr_accessor :remember_token, :activation_token, :reset_token
   before_save   :downcase_email
   before_create :create_activation_digest
   validates :name,  presence: true, length: { maximum: 50 }
@@ -54,7 +54,7 @@ class User < ActiveRecord::Base
         redirect_to login_url
       end
     end
-    
+
 def activate
     update_attribute(:activated,    true)
     update_attribute(:activated_at, Time.zone.now)
@@ -64,6 +64,21 @@ def activate
   def send_activation_email
     UserMailer.account_activation(self).deliver_now
   end
+
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_attribute(:reset_digest,  User.digest(reset_token))
+    update_attribute(:reset_sent_at, Time.zone.now)
+  end
+
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
+  end
+
     private
 
     # Converts email to all lower-case.
--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+    :tls => true,
+    :address => "smtp.gmail.com",
+    :port => "587",
+    :domain => "wvdm.herokuapp.com",
+    :authentication => :plain,
+    :user_name => "v0ilawes",
+    :password => "007vegeta" 
+  }
